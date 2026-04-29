0e96b9263f6201c05d18f4d911f10285d0f3f1ce
Himanshu Yadav <asherkingpin@gmail.com>
2026-04-29 16:34:39 +0530

    Added ViewSet for Product

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from .models import Product
+from .serializer import ProductSerializer
+from accounts.permissions import IsAdmin, IsAdminOrManager, IsReadOnlyOrAuthenticated
 # Create your views here.
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'destroy']:
+            permission_classes = [
+                IsAdmin,
+            ]
+        elif self.action in ['update', 'partial_update']:
+            permission_classes = [
+                IsAdminOrManager
+            ]
+        else:
+            permission_classes = [
+                IsReadOnlyOrAuthenticated
+            ]
+        
+        return [permission() for permission in permission_classes]
+
